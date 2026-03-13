078cbe7f0a08b5500cfbf9090b432f60cd4ee114
SimKDT <simon.cadet79@hotmail.fr>
2026-03-13 01:20:22 +0100

    Auto schemas formatter

--- formatTranslationFiles.py
+++ formatTranslationFiles.py
@@ -4,6 +4,7 @@ SCRIPT_DIR = os.path.join(os.path.dirname(__file__))
 
 TRANSLATION_FILES_DIR = os.path.join(SCRIPT_DIR, 'data', 'translation_files')
 OUTPUT_FILE = os.path.join(SCRIPT_DIR, 'data', 'translationFiles.json')
+SCHEMAS_DIR = os.path.join(SCRIPT_DIR, 'formatted', r"{key}.schema.json")
 
 translation_files = {}
 for filename in os.listdir(TRANSLATION_FILES_DIR):
@@ -11,7 +12,48 @@ for filename in os.listdir(TRANSLATION_FILES_DIR):
         key = os.path.splitext(filename)[0]
         file_path = os.path.join(TRANSLATION_FILES_DIR, filename)
         with open(file_path, 'r', encoding='utf-8') as f:
-            translation_files[key] = yaml.safe_load(f)
+            data = yaml.safe_load(f)
+
+            # add the data to the centralized json file
+            translation_files[key] = data
+
+
+            # generate the schema file
+
+            # format the title based on the fileName field if it exists, otherwise use the key
+            fileName = data.get('fileName', None)
+            title = f"{fileName}.json Schema" if fileName else f"<{key}>.json Schema"
+
+            # format patternProperties
+            patternProperties = data.get('patternProperties', [])
+            formattedPatternProperties = {}
+            for pattern in patternProperties:
+                formattedPatternProperties[pattern['pattern']] = {
+                    "type": "string",
+                    "description": pattern.get('description', '')
+                }
+
+            keys = data.get('keys', [])
+            properties = {}
+            for k in keys:
+                properties[k['name']] = {
+                    "type": "string",
+                    "description": k.get('description', '')
+                }
+
+            schema = {
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "title": title,
+                "description": data.get('description', ''),
+                "type": "object",
+                "patternProperties": formattedPatternProperties,
+                "properties": properties,
+                "additionalProperties": False,
+            }
+
+            with open(SCHEMAS_DIR.format(key=key), 'w', encoding='utf-8') as schema_file:
+                json.dump(schema, schema_file, indent=2, ensure_ascii=False)
+
 
 for file_key, file_data in translation_files.items():
     # remove unnecessary fields
